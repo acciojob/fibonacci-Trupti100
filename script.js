fc5c1d0f916470a7c4676864a749e60176cf94f3
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-08 10:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
 
-module.exports = fibonacci;
+// Example usage:
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
